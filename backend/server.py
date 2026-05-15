6935c4556aadba8e6cf35ccbd3824b7b1c5798c6
mohammedishan7310-jpg <mohammedishan7310@gmail.com>
2026-05-15 15:11:20 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -519,3 +519,11 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+import uvicorn
+
+if __name__ == "__main__":
+    uvicorn.run(
+        app,
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 10000))
+    )
